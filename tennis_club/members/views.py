8c76ce1e8da92f5f3b5dcb793bc882edbe1383cd
o <nesubludai@gmail.com>
2023-02-01 17:44:32 +0300

    Added first model

--- tennis_club/members/views.py
+++ tennis_club/members/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
 
 
 def members(request):
-    return HttpResponse('Hello world')
+    template = loader.get_template('members.html')
+    return HttpResponse(template.render())
