3fe5df67ed96b3208e6acbe12008113a9aea79e5
o <nesubludai@gmail.com>
2023-02-01 18:16:10 +0300

    Configure model member

--- tennis_club/members/models.py
+++ tennis_club/members/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class Member(models.Model):
     first_name = models.CharField(max_length=255)
     second_name = models.CharField(max_length=255)
+    phone = models.IntegerField(null=True)
+    joined_data = models.DateField(null=True)
