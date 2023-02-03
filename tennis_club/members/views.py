aba8e6edfdf8f4be7a629b8fe8a2562838888b1a
o <nesubludai@gmail.com>
2023-02-03 16:49:39 +0300

    Added details about all members

--- tennis_club/members/views.py
+++ tennis_club/members/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
+from .models import Member
 
 
 def members(request):
-    template = loader.get_template('members.html')
-    return HttpResponse(template.render())
+    my_members = Member.objects.all().values()
+    template = loader.get_template('all_members.html')
+    context = {
+        'my_members': my_members,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def details(request, id):
+    my_member = Member.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = {
+        'my_member': my_member,
+    }
+    return HttpResponse(template.render(context, request))
