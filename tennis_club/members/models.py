df1ae3f2670046f612686ac33e9c5212715bb9b5
o <nesubludai@gmail.com>
2023-02-03 16:55:59 +0300

    Template update

--- tennis_club/members/models.py
+++ tennis_club/members/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Member(models.Model):
-    first_name = models.CharField(max_length=255)
-    second_name = models.CharField(max_length=255)
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
     phone = models.IntegerField(null=True)
     joined_data = models.DateField(null=True)
--- tennis_club/members/views.py
+++ tennis_club/members/views.py
@@ -1,22 +1,21 @@
-from django.shortcuts import render
-from django.template import loader
 from django.http import HttpResponse
+from django.template import loader
 from .models import Member
 
 
 def members(request):
-    my_members = Member.objects.all().values()
+    mymembers = Member.objects.all().values()
     template = loader.get_template('all_members.html')
     context = {
-        'my_members': my_members,
+        'mymembers': mymembers,
     }
     return HttpResponse(template.render(context, request))
 
 
 def details(request, id):
-    my_member = Member.objects.get(id=id)
+    mymember = Member.objects.get(id=id)
     template = loader.get_template('details.html')
     context = {
-        'my_member': my_member,
+        'mymember': mymember,
     }
     return HttpResponse(template.render(context, request))
